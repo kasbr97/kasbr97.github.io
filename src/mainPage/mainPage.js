ee2800378c11946d6014518d42242fcd06566b54
kasbr97 <kev.asbr@gmail.com>
2024-01-28 21:26:43 -0800

    Getting started

--- src/mainPage/mainPage.js
+++ src/mainPage/mainPage.js
@@ -4,7 +4,7 @@ function MainPage() {
     return (
         <div className="Container">
             <h2>Hi! my name is Kevin Burga</h2>
-            <div className="Description">
+            <div className="Main-description">
                 <p className="Description-text">
                     I'm a Software engineer with 1 year experience as a Front-end developer using React with TypeScript. 
                     With additional experience experience in Python, SQL, noSQL(mongoDB), Java.
