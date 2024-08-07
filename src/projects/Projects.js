0f0d35b8fc18997b1756fe15eb123650d2e3f51e
kasbr97 <kev.asbr@gmail.com>
2024-08-07 14:55:44 -0700

    Added a scrollable div in Projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,27 +3,49 @@ import "./projects.css"
 function Projects() {
     return (
         <>
-            <div className="App-header">
-                
-                <div className="project-card">
-                    <div className="image-section">
-                        <img src="./img-projects/jokeApp.png" width={"100%"}/>
+            <div className="container-projects">
+                <div className="cards-parent">
+                    <div className="cards-display">
+                        <div className="project-card">
+                            <div className="image-section">
+                                <img alt="Project" src="./img-projects/jokesapp.png" width={"100%"}/>
+                            </div>
+                            <div className="card-body">
+                                <p>JokeApp</p>
+                                <p>It's a Web Application built on C# with ASP .Net MVC. The JokesApp is a simple 
+                                    application that uses the Entity Framework ORM and connects to a database 
+                                    given the connection string. The users can create an account and start adding 
+                                    jokes to the application, which will be saved into a relational database. The jokes can 
+                                    then be edited, listed and deleted; however, only the user that created the 
+                                    joke, can edit and delete it, the other users can only read the joke.</p>
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="image-section">
+                                <img alt="Project" src="./img-projects/jokesapp.png" width={"100%"}/>
+                            </div>
+                            <div className="card-body">
+                                <p>PokemonApp</p>
+                                <p>It's the frontend for a web application that runs in React with Nextjs. 
+                                    It connects to an external API to get all the Pokemon data and displays
+                                    the information and in the website.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="image-section">
+                                <img alt="Project" src="./img-projects/jokesapp.png" width={"100%"}/>
+                            </div>
+                            <div className="card-body">
+                                <p>JokeApp</p>
+                                <p>It's a Web Application built on C# with ASP .Net MVC. The JokesApp is a simple 
+                                    application that uses the Entity Framework ORM and connects to a database 
+                                    given the connection string. The users can create an account and start adding 
+                                    jokes to the application, which will be saved into a relational database.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        <p>Project 1</p>
-                        <p>Web Application built on C# with ASP .Net MVC. The JokesApp is a simple 
-                            application that uses the Entity Framework ORM and connects to a database 
-                            given the connection string. The users can create an account and start adding 
-                            jokes to the application, which will be saved into the database. The jokes can 
-                            then be edited, listed and deleted; however, only the user that created the 
-                            joke, can edit and delete it, the other users can only read the joke.</p>
-                    </div>
-                </div>
-                <div className="project-card">
-                    <div className="image-section">
 
-                    </div>
-                    Project 2
                 </div>
             </div>
         </>
