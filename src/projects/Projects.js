7307db9a9ce286411e41ab4f729f232011e5b351
kasbr97 <kev.asbr@gmail.com>
2024-08-07 16:10:36 -0700

    Added frontend challange information card

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -8,17 +8,6 @@ function Projects() {
                     <div className="cards-display">
                         <div className="project-card">
                             <div className="image-section">
-                                <img alt="Project" src="./img-projects/jokesapp.png" width={"100%"}/>
-                            </div>
-                            <div className="card-body">
-                                <p>Frontend Challenge</p>
-                                <p>It's a small project where I learned how to use Responsive Design,
-                                    added special fonts to the web page, and testing using Jest. 
-                                </p>
-                            </div>
-                        </div>
-                        <div className="project-card">
-                            <div className="image-section">
                                 <img alt="Project" src="./img-projects/pokemonapp.png" width={"100%"}/>
                             </div>
                             <div className="card-body">
@@ -26,6 +15,17 @@ function Projects() {
                                 <p>It's the frontend for a web application that runs in React with Nextjs. 
                                     It connects to an external API to get all the Pokemon data and displays
                                     the information and in the website.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="image-section">
+                                <img alt="Project" src="./img-projects/frontend-challenge.png" width={"100%"}/>
+                            </div>
+                            <div className="card-body">
+                                <p>Frontend Challenge</p>
+                                <p>It's a small project where I learned how to use Responsive Design,
+                                    added special fonts to the web page, and testing using Jest. 
                                 </p>
                             </div>
                         </div>
